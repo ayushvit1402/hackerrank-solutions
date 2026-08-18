69eb60939ae60fb7a8b6b0182831fe5db24da916
Ayush Kumar Mishra <ayushvit1402@gmail.com>
2026-08-18 22:12:53 +0530

    ✅ sWAP cASE [python3] — approach1

---
+++ solutions/python3/easy/swap-case/solution-approach1.py
@@ -0,0 +1,14 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/swap-case/problem?isFullScreen=true
+# Problem     sWAP cASE
+# Difficulty  Easy
+# Subdomain   Strings
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-08-18, 10:12 p.m.
+# ──────────────────────────────────────────────────
+
+def swap_case(s):
+    return s.swapcase()
+
